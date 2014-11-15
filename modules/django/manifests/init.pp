6b1b5325d70737f2bcffa3a7f0667c2242ab377c
Steven Weston <steve.weston@triniplex.com>
2014-11-15 14:51:30 -0800

    Adding pip requirements

--- modules/django/manifests/init.pp
+++ modules/django/manifests/init.pp
@@ -1,5 +1,14 @@
-class django {
-  file { '/tmp/helloModule':
-    content => "Hello World!\n"
-  }
+class django ($user = steven) {
+    package {"python-django":
+        ensure => "installed",
+        }
+    package {"sqlite3":
+        ensure => "installed",
+    }
+    exec {"/usr/bin/django-admin startproject www":
+        require => Package['python-django'],
+        creates => "/home/$user/www/www",
+        cwd => "/home/$user",
+        user => $user,
+    }
 }
